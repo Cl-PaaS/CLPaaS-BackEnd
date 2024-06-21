e631a433ed0938f8634270062eec2342ac43ff89
tmddus2 <tmddus2__@naver.com>
2024-06-21 15:03:52 +0900

    Init: init project

---
+++ src/main/java/cloudclub/clpaas_backend/ClpaasBackendApplication.java
@@ -0,0 +1,13 @@
+package cloudclub.clpaas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClpaasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClpaasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloudclub/clpaas_backend/ClpaasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cloudclub.clpaas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClpaasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
