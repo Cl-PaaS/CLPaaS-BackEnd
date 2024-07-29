6a78f6360c886631f19a31281df2a969f162c663
yubin <garmalhy1@naver.com>
2024-07-29 17:46:28 +0900

    Feat: 단축 URL 원본주소 확인 (#3)

--- src/main/java/com/cloud/cl_paas/domain/validator/controller/ValidatorController.java
+++ src/main/java/com/cloud/cl_paas/domain/validator/controller/ValidatorController.java
@@ -4,6 +4,7 @@ import com.cloud.cl_paas.domain.parser.dto.ReqMessageDto;
 import com.cloud.cl_paas.domain.parser.dto.RespMessageDto;
 import com.cloud.cl_paas.domain.parser.service.ParserService;
 import com.cloud.cl_paas.domain.validator.service.SafeBrowsingService;
+import com.cloud.cl_paas.domain.validator.service.UrlExpanderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,13 +18,17 @@ public class ValidatorController {
 
     private final ParserService parserService;
     private final SafeBrowsingService safeBrowsingService;
+    private final UrlExpanderService urlExpanderService;
 
     @PostMapping("/check")
     public String checkUrlSafety(@RequestBody ReqMessageDto reqMessageDto) {
         RespMessageDto parsedData = parserService.getWords(reqMessageDto);
 
+        // 단축 URL의 원본 주소 확인
+        String originalUrl = urlExpanderService.expandUrl(parsedData.getUrl());
+        log.info("originalUrl = {}", originalUrl);
+        // URL의 안전 여부 확인
         String safeStatus = safeBrowsingService.checkUrlSafety(parsedData.getUrl());
-//        String backLinks = safeBrowsingService.getBackLinks(parsedData.getUrl());
 
             return safeStatus;
     }
