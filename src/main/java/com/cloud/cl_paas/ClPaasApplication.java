33fb4c7e5ef4467d2ea7ef87165fe787dc5cffd4
yubin <garmalhy1@naver.com>
2024-08-05 12:32:43 +0900

    init: re init for proj

--- src/main/java/com/cloud/cl_paas/ClPaasApplication.java
+++ src/main/java/com/cloud/cl_paas/ClPaasApplication.java
@@ -1,13 +1,13 @@
-package cloudclub.clpaas_backend;
+package com.cloud.cl_paas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClpaasBackendApplication {
+public class ClPaasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClpaasBackendApplication.class, args);
+		SpringApplication.run(ClPaasApplication.class, args);
 	}
 
 }
--- src/test/java/com/cloud/cl_paas/ClPaasApplicationTests.java
+++ src/test/java/com/cloud/cl_paas/ClPaasApplicationTests.java
@@ -1,10 +1,10 @@
-package cloudclub.clpaas_backend;
+package com.cloud.cl_paas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ClpaasBackendApplicationTests {
+class ClPaasApplicationTests {
 
 	@Test
 	void contextLoads() {
