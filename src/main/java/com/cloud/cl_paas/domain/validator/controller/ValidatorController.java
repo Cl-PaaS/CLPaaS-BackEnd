469934c5b557c2cb92a703be40b00da937e8e895
yubin <garmalhy1@naver.com>
2024-08-06 17:35:37 +0900

    feat: 피시탱크 api

--- src/main/java/com/cloud/cl_paas/domain/validator/controller/ValidatorController.java
+++ src/main/java/com/cloud/cl_paas/domain/validator/controller/ValidatorController.java
@@ -3,6 +3,7 @@ package com.cloud.cl_paas.domain.validator.controller;
 import com.cloud.cl_paas.domain.parser.dto.ReqMessageDto;
 import com.cloud.cl_paas.domain.parser.dto.RespMessageDto;
 import com.cloud.cl_paas.domain.parser.service.ParserService;
+import com.cloud.cl_paas.domain.validator.service.PhishtankValidateService;
 import com.cloud.cl_paas.domain.validator.service.SafeBrowsingService;
 import com.cloud.cl_paas.domain.validator.service.UrlExpanderService;
 import lombok.RequiredArgsConstructor;
@@ -18,17 +19,20 @@ public class ValidatorController {
 
     private final ParserService parserService;
     private final SafeBrowsingService safeBrowsingService;
+    private final PhishtankValidateService phishtankValidateService;
     private final UrlExpanderService urlExpanderService;
 
     @PostMapping("/check")
     public String checkUrlSafety(@RequestBody ReqMessageDto reqMessageDto) {
         RespMessageDto parsedData = parserService.getWords(reqMessageDto);
-
         // 단축 URL의 원본 주소 확인
         String originalUrl = urlExpanderService.expandUrl(parsedData.getUrl());
         log.info("originalUrl = {}", originalUrl);
+        log.info("parsedData.getUrl() = {}", parsedData.getUrl());
         // URL의 안전 여부 확인
-        String safeStatus = safeBrowsingService.checkUrlSafety(parsedData.getUrl());
+//        String safeStatus = safeBrowsingService.checkUrlSafety(parsedData.getUrl());
+        // 피시 탱크를 통한 안전 여부 확인
+        String safeStatus = phishtankValidateService.checkUrlSafety(parsedData.getUrl());
 
             return safeStatus;
     }
